d4ecdb8080bd1cde59151e75c27fefaba7c738df
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-07-11 00:55:10 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className='text-jsouls-blue font-jsouls-primary'> hello </h1>
+      <i className= "bi bi-layout-text-window text-indigo-500"></i>
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+],
+  theme: {
+    extend: {colors: {
+      'jsouls-blue': '#3761ee',
+      'jsouls-blue-bg': '#ebeffd',
+    },
+    fontFamily: {
+      'jsouls-primary': ['Poppins']
+    }
+
+},
+  },
+  plugins: [],
+}
